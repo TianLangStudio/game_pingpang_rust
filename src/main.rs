8928cff390a2ec17d233d70d23cb016b9df94f3d
Tianlang <tianlangstuido@aliyun.com>
2019-10-24 23:02:25 +0800

    lesson4-draw-ball

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,39 @@
 //extern crate piston_window;
 use piston_window::*;
 use game_pingpang::tianlangstudio::*;
-fn draw(ctx: Context, gph: &mut impl Graphics, racket: &Racket) {
+fn draw(ctx: Context, gph: &mut impl Graphics, racket: &Racket, ball: &Ball) {
             clear([1.0; 4], gph);
             racket.draw(ctx, gph);
+            ball.draw(ctx, gph);
 }
 fn main() {
+    let win_width = 640;
+    let win_height = 480;
     let mut window: PistonWindow =
-        WindowSettings::new("Game Pingpang", [640, 480])
+        WindowSettings::new("Game Pingpang", [win_width, win_height])
         .exit_on_esc(true).build().unwrap();
     let mut racket = Racket::new();
+    let mut ball = Ball::new(win_width as f64, win_height as f64);
     while let Some(event) = window.next() {
         if let Some(Button::Keyboard(key)) = event.press_args() {
             if let Key::Left = key {
                racket.mv_left();
             }
             if let Key::Right = key {
-               racket.mv_right(640.0);
+               racket.mv_right(win_width as f64);
             }
         }
+        if let Some(_) = event.render_args() {
+            if ball.is_knock_top() ||
+                ball.is_knock_left() ||
+                    ball.is_knock_right(win_width as f64) ||
+                    ball.is_knock_racket(&racket) {
+                 ball.rebound();
+            }
+            ball.mv();
+        }
         window.draw_2d(&event, |context, graphics, _device| {
-            draw(context, graphics, &racket);
+            draw(context, graphics, &racket, &ball);
         });
     }
 }
